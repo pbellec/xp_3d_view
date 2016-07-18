04147a2d1ee8fe9c15b46171e5314ceb17cff505
Pierre Bellec <pierre.bellec@gmail.com>
2016-07-18 00:46:04 -0400

    A first prototype. Sagital and coronal views work, axial will need a bit of work.

---
+++ xp_3d_view.m
@@ -0,0 +1,20 @@
+
+%% Read html template
+hf = fopen('xp_3d_view_template.html');
+text_html = fread(hf,Inf,'uint8=>char')';
+fclose(hf);
+
+%% Read png
+%hf = fopen ('test_lowres.png');
+hf = fopen ('test.png');
+data = fread (hf,Inf,'double');
+fclose(hf);
+
+%% Convert to base 64
+str = base64_encode(data);
+text_html = strrep(text_html,'$DATA',str);
+
+%% Write page 
+hf = fopen('xp_3d_view.html','w');
+fprintf(hf,'%s',text_html);
+fclose(hf);
